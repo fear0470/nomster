248129c817a0759d06f4ab6418644d0034ff72f8
Nicholas Fearns <you@example.com>
2019-03-12 19:04:21 +0000

    working on picture modal form

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,17 +1,27 @@
 class PhotosController < ApplicationController
-  before_action :authenticate_user!
-
+  
   def create
     @place = Place.find(params[:place_id])
-    @place = Photo.create(photo_params.merge(user: current_user))
+    @place.photos.create(photo_params.merge(user: current_user))
+    if @place.valid?
+      redirect_to place_path(@place)
+    else 
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  def destroy 
+    @place = Place.find(params[:place_id])
+    @photo = Photo.find(params[:id])
+    if @photo.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+    
+    @photo.destroy
     redirect_to place_path(@place)
   end
-
-private
-
+  
   def photo_params
     params.require(:photo).permit(:caption, :picture)
   end
-
-
 end
