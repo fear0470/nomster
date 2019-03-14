7cc15b32d881ea46040d8e13614f56537deeb467
Nicholas Fearns <you@example.com>
2019-03-14 17:48:05 +0000

    working on user comments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
     password: ENV['GMAIL_PASSWORD']
   }
 
-  config.action_mailer.default_url_options = {host: 'localhost:3030'}
+  config.action_mailer.default_url_options = {host: 'lhttps://nomster-nicholas-fearns.herokuapp.com/'}
   
   # Settings specified here will take precedence over those in config/application.rb.
 
